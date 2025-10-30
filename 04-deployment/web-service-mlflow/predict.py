66629347a32837719e150ae8969365690d91c9b3
Kenneth Leo <kenneth.m.leo@gmail.com>
2025-10-30 18:31:40 +0800

    finish 4.1 and 4.2

---
+++ 04-deployment/web-service-mlflow/predict.py
@@ -0,0 +1,43 @@
+import pickle
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
+from flask import Flask, request, jsonify
+
+MLFLOW_TRACKING_URI = 'http://127.0.0.1:5000'
+RUN_ID = '76d8c5bd734e4bed8cb4520ca2500496'
+
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+logged_model = f'runs:/{RUN_ID}/model'
+model = mlflow.pyfunc.load_model(logged_model)   
+
+def prepare_features(ride):
+    features = {}
+    features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+def predict(features):
+    preds = model.predict(features)
+    return float(preds[0])
+
+
+app = Flask('duration-prediction')
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json() #get data from request
+
+    features = prepare_features(ride)
+    pred = predict(features)
+
+    result = {
+        'duration': pred,
+        'model_version': RUN_ID
+    }
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
